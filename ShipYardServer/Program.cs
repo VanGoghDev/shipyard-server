1047c1de7a12ad79f86187ad2a73b73a2f606c04
Cyril Firsov <kifirigor@gmail.com>
2023-06-18 23:58:31 +0300

    JWT Added, EF Identity Context migration

--- ShipYardServer/Program.cs
+++ ShipYardServer/Program.cs
@@ -1,8 +1,28 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using ShipYardServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ShipYardServer.ViewModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("SqlServerDb")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    //! Change your security Policy here to suits your policy
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    //! tutorial purposes im allowed users to sign in without their email confirmation
+    options.SignIn.RequireConfirmedEmail = false;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+SetUpBearerJwtAuth(builder);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,7 +39,39 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
+
+static void SetUpBearerJwtAuth(WebApplicationBuilder builder)
+{
+    //Fetch JWT configuration from appsettings.json
+    var jwtSection = builder.Configuration.GetSection("jwtBearerTokenSettings");
+    //Parse jwtSection from appsettings.json into Concrete Class "JWTBearerTokenSettings"
+    builder.Services.Configure<JWTBearerTokenSettings>(jwtSection);
+    var jwtBearerTokenSettings = jwtSection.Get<JWTBearerTokenSettings>();
+    var key = Encoding.ASCII.GetBytes(jwtBearerTokenSettings.SecretKey);
+
+    builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtBearerTokenSettings.Issuer,
+                ValidateAudience = true,
+                ValidAudience = jwtBearerTokenSettings.Audience,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+}
---
+++ ShipYardServer/ViewModels/JWTBearerTokenSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ShipYardServer.ViewModels;
+
+public class JWTBearerTokenSettings
+{
+    public string SecretKey { get; set; }
+    public string Audience { get; set; }
+    public string Issuer { get; set; }
+    public int ExpiryTimeInMinutes { get; set; }
+}
