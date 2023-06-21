287685de4f3cc6fccf91466250b02775581afda9
Cyril Firsov <kifirigor@gmail.com>
2023-06-21 23:45:23 +0300

    Guidelines for future authentication service implementation.

--- Shipyard.Application/Services/AuthService.cs
+++ Shipyard.Application/Services/AuthService.cs
@@ -13,9 +13,19 @@ public class AuthService : IAuthService
     
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        // 1. Check if user with given email exists.
+        
+        // 2. Check if given password equals user password.
+        
+        // 3. Save user in db.
+        
+        // 4. Return jwt token.
+        
         var id = Guid.NewGuid();
 
         var token = _jwtTokenGenerator.GenerateToken(id, firstName, lastName);
+        
+        // Make AuthenticationResult return user object and token in order to have flexible control on modifying claims.
         return new AuthenticationResult(
             Guid.NewGuid(),
             firstName,
@@ -26,6 +36,12 @@ public class AuthService : IAuthService
     
     public AuthenticationResult Login(string email, string password)
     {
+        // 1. Check if user with given email exists in db.
+        
+        // 2. Check if given password equals user password.
+        
+        // 3. Return jwt token.
+        
         return new AuthenticationResult(
             Guid.NewGuid(),
             "firstName",
